c6058c9d37ed0f7047a3a217b99140cd35e3e83f
Sanhu Li <lisanhu2014@hotmail.com>
2023-12-08 16:58:44 -0500

    Adding RppBillAllMechPatch, which is a patch to Robots++ Mod to fix AnyMech in Bill settings not working for Robots++ robots.

---
+++ 07-RPP_Bill_AllMech_Patch/Source/Main.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using Verse.Sound;
+using Verse.Noise;
+using Verse.Grammar;
+using RimWorld;
+using RimWorld.Planet;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using System.Reflection;
+
+// using System.Reflection;
+// using HarmonyLib;
+
+namespace RPP_Patch
+{
+    [StaticConstructorOnStartup]
+    public static class LoadingScreen
+    {
+        static LoadingScreen()
+        {
+            Log.Warning("RPP_Bill_AllMech_Patch Loaded");
+
+            var harmony = new Harmony("com.RunningBugs.RPP_Bill_AllMech_Patch");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+    
+    [HarmonyPatch(typeof(Bill), "PawnAllowedToStartAnew")]
+    class RobotsPP_Bill_Patch_PawnAllowedToStartAnew
+    {
+        private static bool Check(Pawn p, Bill b)
+        {
+            if (b.recipe.workSkill != null && (p.skills != null || p.IsColonyMech))
+            {
+                int num = ((p.skills != null) ? p.skills.GetSkill(b.recipe.workSkill).Level : p.RaceProps.mechFixedSkillLevel);
+                if (num < b.allowedSkillRange.min)
+                {
+                    JobFailReason.Is("UnderAllowedSkill".Translate(b.allowedSkillRange.min), b.Label);
+                    return false;
+                }
+                if (num > b.allowedSkillRange.max)
+                {
+                    JobFailReason.Is("AboveAllowedSkill".Translate(b.allowedSkillRange.max), b.Label);
+                    return false;
+                }
+            }
+            if (ModsConfig.BiotechActive && b.recipe.mechanitorOnlyRecipe && !MechanitorUtility.IsMechanitor(p))
+            {
+                JobFailReason.Is("NotAMechanitor".Translate());
+                return false;
+            }
+            return true;
+        }
+
+        static void Postfix(Pawn p, Bill __instance, ref bool __result)
+        {
+            if (!p.IsColonyMech)
+            {
+                return;
+            }
+            if (__instance.PawnRestriction != null)
+            {
+                return;
+            }
+            if (__instance.SlavesOnly && !p.IsSlave)
+            {
+                return;
+            }
+            if (__instance.MechsOnly && !p.IsColonyMechPlayerControlled)
+            {
+                if (p.def.defName.StartsWith("RPP_Bot_"))
+                {
+                    __result = Check(p, __instance);
+                    return;
+                }
+            }
+        }
+
+    }
+    
+    public static class Log
+    {
+        const string prefix = "[RPP_Patch] ";
+        public static void Warning(string msg, [CallerFilePath] string fileName = "", [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            Verse.Log.Warning(prefix + $"[ {fileName}:{lineNumber} {memberName} ]" + msg);
+        }
+    }
+
+}
