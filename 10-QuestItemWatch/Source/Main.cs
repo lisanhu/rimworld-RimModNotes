a2ac05681fe9ff63dae0522475abc19e53f24241
Sanhu Li <lisanhu2014@hotmail.com>
2023-12-20 14:40:21 -0500

    Adding a patch to build roads on ice sheet and sea ice

---
+++ 10-QuestItemWatch/Source/Main.cs
@@ -0,0 +1,39 @@
+﻿using Verse;
+using RimWorld;
+using Logger;
+using Log = Logger.Log;
+
+// using System.Reflection;
+// using HarmonyLib;
+
+namespace QuestItemWatch
+{
+    [DefOf]
+    public class TemplateDefOf
+    {
+        public static LetterDef success_letter;
+    }
+
+    public class QuestItemWatcher : GameComponent 
+    {
+        public QuestItemWatcher(Game game) { }
+
+        public override void GameComponentTick()
+        {
+            base.GameComponentTick();
+            Find.QuestManager.
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class Start
+    {
+        static Start()
+        {
+            Log.Message("QuestItemWatch loaded successfully!");
+            var harmony = new HarmonyLib.Harmony("com.runningbugs.questitemwatch");
+            harmony.PatchAll();
+        }
+    }
+
+}
---
+++ 11-RoadOnIcePatch/Source/Main.cs
@@ -0,0 +1,32 @@
+﻿using Verse;
+using RimWorld;
+
+using Log = Logger.Log;
+// using System.Reflection;
+// using HarmonyLib;
+
+namespace RoadOnIce
+{
+    public class MyMapComponent : MapComponent
+    {
+        public MyMapComponent(Map map) : base(map){}
+        public override void FinalizeInit()
+        {
+            base.FinalizeInit();
+            BiomeDefOf.IceSheet.allowRoads = true;
+            BiomeDefOf.SeaIce.allowRoads = true;
+            Log.Message("FinalizeInit");
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class Start
+    {
+        static Start()
+        {
+            Log.prefix = "RoadOnIce";
+            Log.Message("loaded successfully!");
+        }
+    }
+
+}
