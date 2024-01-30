3f91e32c74cc326d37f57dca1bac149725f097fc
Sanhu Li <lisanhu2014@hotmail.com>
2024-01-30 03:07:00 -0500

    Adding translation for HakuroXenohuman mod, far from finished Adding FacialAnimationEyeFix

---
+++ 19-FacialAnimationEyeFix/Source/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using Verse.Sound;
+using Verse.Noise;
+using Verse.Grammar;
+using RimWorld;
+using RimWorld.Planet;
+
+using Logs = Logger.Log;
+
+// using System.Reflection;
+// using HarmonyLib;
+
+namespace FacialAnimationEyeFix
+{
+    public class TextureReDraw : GameComponent {
+        public TextureReDraw(Game game) {
+        }
+
+        public override void FinalizeInit() {
+            FacialAnimation.MyGraphicPool.RepaintAllGraphic();
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class Start
+    {
+        static Start()
+        {
+            Logs.prefix = "FacialAnimationEyeFix";
+            Logs.Warning("Mod loaded!");
+        }
+    }
+
+}
