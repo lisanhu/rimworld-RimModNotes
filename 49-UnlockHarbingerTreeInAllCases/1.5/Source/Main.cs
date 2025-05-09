0aaf0981abd1afc9714375c364d9cfb9f892810d
Sanhu Li <lisanhu2014@hotmail.com>
2025-05-09 16:12:39 -0400

    adding some translation, some updates on the patch

--- 49-UnlockHarbingerTreeInAllCases/1.5/Source/Main.cs
+++ 49-UnlockHarbingerTreeInAllCases/1.5/Source/Main.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
 using HarmonyLib;
@@ -29,10 +29,27 @@ namespace UnlockHarbingerTreeInAllCases
         public static bool Prefix(IncidentWorker_HarbingerTreeSpawn __instance)
         {
             // Whenever this incident is triggered, unlock the Harbinger Tree entry in the Entity Codex
-            Find.EntityCodex.SetDiscovered(MyEntityCodexEntryDefOf.HarbingerTree);
+            if (!Find.EntityCodex.Discovered(MyEntityCodexEntryDefOf.HarbingerTree))
+            {
+                Find.EntityCodex.SetDiscovered(MyEntityCodexEntryDefOf.HarbingerTree);
+                Messages.Message("HarbingerTreeForcedDiscovered".Translate(), MessageTypeDefOf.NeutralEvent, false);
+            }
 
             // Continue with the original method
             return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(IncidentWorker_SpecialTreeSpawn), "CanFireNowSub")]
+    public static class Patch_IncidentWorker_SpecialTreeSpawn_CanFireNowSub
+    {
+        public static void Postfix(IncidentParms parms, IncidentWorker_SpecialTreeSpawn __instance, ref bool __result)
+        {
+            // Making this event always triggerable if it's a Harbinger Tree event on a extreme biome map when result is false
+            if (!__result && __instance is IncidentWorker_HarbingerTreeSpawn && parms.target is Map m && m.Biome.isExtremeBiome)
+            {
+                __result = true;
+            }
         }
     }
 }
