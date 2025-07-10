f0219923670773190aee59db3adc724557ffa2c6
Sanhu Li <lisanhu2014@hotmail.com>
2025-07-10 08:59:25 -0400

    Adding 56

--- 56-AllowRightClickWorkOutsideZone/1.6/Source/Main.cs
+++ 56-AllowRightClickWorkOutsideZone/1.6/Source/Main.cs
@@ -5,27 +5,49 @@ using System;
 using System.Linq;
 using Verse.AI;
 using Verse.AI.Group;
+using System.Reflection;
 
 namespace ARCWOZ;
 
-public class FloatMenuOptionProvider_AreaBypass : FloatMenuOptionProvider_WorkGivers
+public class FloatMenuOptionProvider_BypassRestrictions : FloatMenuOptionProvider_WorkGivers
 {
+    private DefMap<WorkTypeDef, int> GetWorkPrioritiesSettings(Pawn pawn, IEnumerable<WorkTypeDef> workTypes)
+    {
+        var priorities = new DefMap<WorkTypeDef, int>();
+        foreach (var workType in workTypes)
+        {
+            priorities[workType] = pawn.workSettings.GetPriority(workType);
+        }
+        return priorities;
+    }
+
     public override IEnumerable<FloatMenuOption> GetOptions(FloatMenuContext context)
     {
-        // If current mouse position is inside our current area, we should skip this
         var pawn = context.FirstSelectedPawn;
-        if (context.ClickedCell.InAllowedArea(pawn))
-        {
-            return [];
-        }
+        // Find all worktypes that the pawn can do
+        var worktypes = DefDatabase<WorkTypeDef>.AllDefsListForReading.Where(wt => !pawn.WorkTypeIsDisabled(wt));
+        // backup the pawn worktype settings and area restriction
+        var backupWorkSettings = GetWorkPrioritiesSettings(pawn, worktypes);
         var backupAreaRestriction = pawn.playerSettings.AreaRestrictionInPawnCurrentMap;
+
+
+        // Set all worktypes to enabled
+        foreach (var worktype in worktypes)
+        {
+            pawn.workSettings.SetPriority(worktype, 3); // Set to default priority
+        }
         pawn.playerSettings.AreaRestrictionInPawnCurrentMap = null; // Set to allow all areas
         var options = base.GetOptions(context).ToList();
-        pawn.playerSettings.AreaRestrictionInPawnCurrentMap = backupAreaRestriction; // Restore original area restriction
-
         foreach (var option in options)
         {
-            option.Label += " [Bypass Area]";
+            option.Label += " [Bypass Restrictions]";
+        }
+
+        // Restore the original area and worktype settings
+        pawn.playerSettings.AreaRestrictionInPawnCurrentMap = backupAreaRestriction; // Restore original area restriction
+        foreach (var priorities in backupWorkSettings)
+        {
+            pawn.workSettings.SetPriority(priorities.Key, priorities.Value);
         }
         return options;
     }
@@ -33,19 +55,31 @@ public class FloatMenuOptionProvider_AreaBypass : FloatMenuOptionProvider_WorkGi
     public override IEnumerable<FloatMenuOption> GetOptionsFor(Thing clickedThing, FloatMenuContext context)
     {
         var pawn = context.FirstSelectedPawn;
-        if (context.ClickedCell.InAllowedArea(pawn))
-        {
-            return [];
-        }
+        // Find all worktypes that the pawn can do
+        var worktypes = DefDatabase<WorkTypeDef>.AllDefsListForReading.Where(wt => !pawn.WorkTypeIsDisabled(wt));
+        // backup the pawn worktype settings and area restriction
+        var backupWorkSettings = GetWorkPrioritiesSettings(pawn, worktypes);
         var backupAreaRestriction = pawn.playerSettings.AreaRestrictionInPawnCurrentMap;
+
+        // Set all worktypes to enabled
+        foreach (var worktype in worktypes)
+        {
+            pawn.workSettings.SetPriority(worktype, 3); // Set to default priority
+        }
         pawn.playerSettings.AreaRestrictionInPawnCurrentMap = null; // Set to allow all areas
         var options = base.GetOptionsFor(clickedThing, context).ToList();
-        pawn.playerSettings.AreaRestrictionInPawnCurrentMap = backupAreaRestriction; // Restore original area restriction
-
         foreach (var option in options)
         {
-            option.Label += " [Bypass Area]";
+            option.Label += " [Bypass Restrictions]";
         }
+
+        // Restore the original worktype settings and area restriction
+        pawn.playerSettings.AreaRestrictionInPawnCurrentMap = backupAreaRestriction; 
+        foreach (var priorities in backupWorkSettings)
+        {
+            pawn.workSettings.SetPriority(priorities.Key, priorities.Value);
+        }
+
         return options;
     }
 }
---
+++ 59-GhoulCommands/1.6/Source/Main.cs
@@ -0,0 +1,169 @@
+﻿using Verse;
+using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse.AI;
+
+namespace GhoulCommands;
+
+public class GhoulCommandsSettings : ModSettings
+{
+    public bool enableJuggernautSerumToggle = true;
+    // public bool enableMetalbloodSerumToggle = true;
+
+    public override void ExposeData()
+    {
+        base.ExposeData();
+        Scribe_Values.Look(ref enableJuggernautSerumToggle, "GhoulCommands.enableJuggernautSerumToggle", true);
+        // Scribe_Values.Look(ref enableMetalbloodSerumToggle, "GhoulCommands.enableMetalbloodSerumToggle", true);
+    }
+}
+
+public class GhoulCommandsModUI : Mod
+{
+    public static GhoulCommandsSettings Settings;
+    public GhoulCommandsModUI(ModContentPack content) : base(content)
+    {
+        Settings = GetSettings<GhoulCommandsSettings>();
+    }
+
+    public override string SettingsCategory() => "GhoulCommands".Translate();
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        Listing_Standard listing = new Listing_Standard();
+        listing.Begin(inRect);
+
+        listing.CheckboxLabeled("GhoulCommands.EnableJuggernautSerumText".Translate(), ref Settings.enableJuggernautSerumToggle, "GhoulCommands.EnableJuggernautSerumDesc".Translate());
+        // listing.CheckboxLabeled("GhoulCommands.EnableMetalbloodSerumText".Translate(), ref Settings.enableMetalbloodSerumToggle, "GhoulCommands.EnableMetalbloodSerumDesc".Translate());
+
+        listing.End();
+    }
+}
+
+public class CompProperties_GhoulCommands : CompProperties
+{
+    public CompProperties_GhoulCommands()
+    {
+        compClass = typeof(CompGhoulCommands);
+    }
+}
+
+public class CompGhoulCommands : ThingComp, IExposable
+{
+    public bool ghoulTakingJuggernautSerum = false;
+
+    public override IEnumerable<Gizmo> CompGetGizmosExtra()
+    {
+        if (parent is Pawn pawn && pawn.IsColonySubhumanPlayerControlled && pawn.IsGhoul)
+        {
+            if (GhoulCommandsModUI.Settings.enableJuggernautSerumToggle)
+            {
+                yield return new Command_Toggle
+                {
+                    defaultLabel = "GhoulCommands.ToggleJuggernautSerumText".Translate(),
+                    defaultDesc = "GhoulCommands.ToggleJuggernautSerumDesc".Translate(),
+                    icon = DefDatabase<ThingDef>.GetNamed("JuggernautSerum").uiIcon,
+                    isActive = () => ghoulTakingJuggernautSerum,
+                    toggleAction = () =>
+                    {
+                        ghoulTakingJuggernautSerum = !ghoulTakingJuggernautSerum;
+                    }
+                };
+            }
+        }
+        else
+        {
+            yield break;
+        }
+    }
+
+    public void ExposeData()
+    {
+        Scribe_Values.Look(ref ghoulTakingJuggernautSerum, "ghoulTakingJuggernautSerum", false);
+    }
+}
+
+[DefOf]
+public static class GhoulCommandsDefOf
+{
+    public static JobDef TakeJuggernautSerum;
+}
+
+public class JobDriver_TakeJuggernautSerum : JobDriver
+{
+    /// <summary>
+    /// Form A => pawn
+    /// B => serum
+    /// </summary>
+    private Thing IngestibleSource => job.GetTarget(TargetIndex.B).Thing;
+
+    private float ChewDurationMultiplier
+	{
+		get
+		{
+			Thing ingestibleSource = IngestibleSource;
+			if (ingestibleSource.def.ingestible != null && !ingestibleSource.def.ingestible.useEatingSpeedStat)
+			{
+				return 1f;
+			}
+			return 1f / pawn.GetStatValue(StatDefOf.EatingSpeed);
+		}
+	}
+    private const TargetIndex SerumInd = TargetIndex.B;
+    protected override IEnumerable<Toil> MakeNewToils()
+    {
+        this.FailOnDestroyedOrNull(SerumInd);
+        this.FailOnBurningImmobile(SerumInd);
+        this.FailOnForbidden(SerumInd);
+
+        yield return Toils_Goto.GotoThing(SerumInd, PathEndMode.Touch)
+            .FailOnSomeonePhysicallyInteracting(SerumInd);
+
+        yield return Toils_Ingest.ChewIngestible(pawn, ChewDurationMultiplier, SerumInd).FailOn((Toil x) =>
+        {
+            return !IngestibleSource.Spawned && (pawn.carryTracker == null || pawn.carryTracker.CarriedThing != IngestibleSource);
+        }).FailOnCannotTouch(SerumInd, PathEndMode.Touch);
+
+        // Toil to use the serum immediately after picking it up
+        yield return Toils_Ingest.FinalizeIngest(pawn, SerumInd);
+    }
+
+    public override bool TryMakePreToilReservations(bool errorOnFailed)
+    {
+        return pawn.Reserve(job.GetTarget(SerumInd), job, 1, -1, null, errorOnFailed);
+    }
+}
+
+public class JobGiver_TakeJuggernautSerum : ThinkNode_JobGiver
+{
+    private static ThingDef JuggerSerumDef => DefDatabase<ThingDef>.GetNamed("JuggernautSerum", true);
+    private static HediffDef JuggerHediffDef => DefDatabase<HediffDef>.GetNamed("JuggernautSerum", true);
+
+    protected override Job TryGiveJob(Pawn pawn)
+    {
+        if (!pawn.Spawned || !pawn.IsGhoul || !pawn.IsColonySubhumanPlayerControlled ||
+            !GhoulCommandsModUI.Settings.enableJuggernautSerumToggle ||
+            pawn.TryGetComp<CompGhoulCommands>()?.ghoulTakingJuggernautSerum != true ||
+            pawn.health.hediffSet.HasHediff(JuggerHediffDef))
+        {
+            return null;
+        }
+
+        Thing serum = GenClosest.ClosestThingReachable(
+            pawn.Position,
+            pawn.Map,
+            ThingRequest.ForDef(JuggerSerumDef),
+            PathEndMode.ClosestTouch,
+            TraverseParms.For(pawn),
+            9999f,
+            t => t.Spawned && !t.IsForbidden(pawn) && pawn.CanReserveAndReach(t, PathEndMode.ClosestTouch, Danger.Deadly));
+
+        if (serum == null)
+        {
+            return null;
+        }
+
+        return JobMaker.MakeJob(GhoulCommandsDefOf.TakeJuggernautSerum, pawn, serum);
+    }
+}
