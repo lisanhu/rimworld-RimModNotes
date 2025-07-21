4e978edda1c3d449197f08bdcdc877fd2f1dce29
Sanhu Li <lisanhu2014@hotmail.com>
2025-07-21 07:57:46 -0400

    add 62

---
+++ 62-PawnNotBlockingConstruct/1.6/Source/Main.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace Template;
+
+
+[StaticConstructorOnStartup]
+public static class Start
+{
+    static Start()
+    {
+        Harmony harmony = new Harmony("com.RunningBugs.Test");
+        harmony.PatchAll();
+
+        Log.Message("Test mod loaded successfully!");
+    }
+}
+
+
+[HarmonyPatch(typeof(GenConstruct), nameof(GenConstruct.BlocksConstruction))]
+public static class GenConstruct_BlocksConstruction_Prefix
+{
+    static void Postfix(Thing constructible, Thing t, ref bool __result)
+    {
+        // Skip pawn blocking entirely
+        if (t is Pawn)
+        {
+            __result = false;
+        }
+    }
+}
